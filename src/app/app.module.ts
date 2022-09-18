460d0d1f046f3c55153f088c8720cbb9e7d3f5a6
Abril D'Angelo <dangelolebrero.abril@gmail.com>
2022-09-18 16:03:26 -0300

    Fix carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,11 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 import { environment } from '../environments/environment';
 import { reducers } from './store/movies.reducers';
 import { MoviesEffects } from './store/movies.effects';
-import { ImagePipe } from './pipes/image.pipe';
 
 @NgModule({
   declarations: [AppComponent],
