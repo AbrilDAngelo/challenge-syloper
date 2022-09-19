b0ce214e9bf51a92c85ea1639cc09fc89ddbab91
Abril D'Angelo <dangelolebrero.abril@gmail.com>
2022-09-19 11:00:10 -0300

    Carousels movie/cast

--- src/app/components/movie-carousel-card/movie-carousel-card.component.ts
+++ src/app/components/movie-carousel-card/movie-carousel-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movie-carousel-card',
   templateUrl: './movie-carousel-card.component.html',
-  styleUrls: ['./movie-carousel-card.component.sass']
+  styleUrls: ['./movie-carousel-card.component.sass'],
 })
 export class MovieCarouselCardComponent implements OnInit {
+  @Input() posterUrl!: string;
+  @Input() title!: string;
+  @Input() release_date!: Date;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
